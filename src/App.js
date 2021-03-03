78f7cc9f362c1b7274d7679a6a65dae9891f41fe
islammusicscv <islam.music@scv.si>
2021-03-03 10:07:42 +0100

    Začetek dela z Reactom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Ocene from "./Ocene";
+
 
 function App() {
+
+  const vrednost = 100;
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pozdravljeni študent.</h1>
+      <Ocene ime="Jure Novak" ocena="10" />
+      <Ocene ime="Gorazd Žižek"/>
+      <Ocene ime="Žan Pukmajster" ocena="10"/>
+      <Ocene ime="Tina Novak"/>
     </div>
   );
 }
---
+++ src/Ocene.js
@@ -0,0 +1,11 @@
+function Ocene(props) {
+    return (
+        <div>
+            <h3>{props.ime}</h3>
+            <h2>Ime predmeta</h2>
+            <h1>{props.ocena}</h1>
+        </div>
+    );    
+}
+
+export default Ocene;
