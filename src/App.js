d77f5fc9485970fdad145890cdd626ec7020e2c1
islammusicscv <islam.music@scv.si>
2021-03-10 10:04:24 +0100

    podatki iz jsona

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
-import React from 'react';
-import Ocene from "./Ocene";
+import React, {useEffect, useState} from 'react';
+//import Ocene from "./Ocene";
+import Uporabniki from "./Uporabniki";
+import axios from 'axios';
 
+
+const studenti = [
+  {
+    ime: "Jure",
+    priimek: "Novak",
+    letnica: "2000",
+    predmet: "ISS",
+    id: "1"
+  },
+  {
+    ime: "Maja",
+    priimek: "Kos",
+    letnica: "2002",
+    predmet: "RAI",
+    id: "2"
+  },
+  {
+    ime: "Tina",
+    priimek: "Volk",
+    letnica: "2000",
+    predmet: "ZPB 1",
+    id: "3"
+  },
+  {
+    ime: "Miha",
+    priimek: "Medved",
+    letnica: "2001",
+    predmet: "PROG",
+    id: "4"
+  },
+];
 
 function App() {
 
-  const vrednost = 100;
+  const [users, setUsers] = useState();
 
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
+      const users = res.data;
+      setUsers(users);
+    });
+  },[]);
+
+  console.log(users);
   
-
   return (
-    <div>
+    <div>      
       <h1>Pozdravljeni študent.</h1>
-      <Ocene ime="Jure Novak" ocena="10" />
-      <Ocene ime="Gorazd Žižek"/>
-      <Ocene ime="Žan Pukmajster" ocena="10"/>
-      <Ocene ime="Tina Novak"/>
+      {
+        /*studenti.map((student) => {
+          //console.log(student);
+          const{ime,priimek,letnica,id,predmet} = student;
+          return (
+            <Ocene key={id} ime={ime} priimek={priimek} predmet={predmet} letnica={letnica} />
+          );
+        })*/
+      }
+      <hr />
+      <h1>Podatki iz drugega strežnika</h1>
+      {
+        users && (
+          users.map((user) => {
+            //console.log(user);
+            const{name,id} = user;
+            return (
+              <Uporabniki key={id} name={name} />
+            );
+          })
+        )
+      }
     </div>
   );
 }
--- src/Ocene.js
+++ src/Ocene.js
@@ -1,11 +1,38 @@
-function Ocene(props) {
+import React,{useState,useEffect} from 'react';
+
+
+
+function Ocene(props) {   
+    const[vrednost,setVrednost] = useState(0);
+    
+    /*useEffect(() => {
+        console.log(vrednost);
+      },[]);*/
+
+    const {ime, predmet, letnica, priimek} = props;
+    
+    
+    function povecaj() {
+        (vrednost<10) && (
+            setVrednost(vrednost+1)
+        );
+    }
+    function pomanjsaj() {
+        (vrednost>1) && (
+            setVrednost(vrednost-1)
+        );
+    }    
     return (
-        <div>
-            <h3>{props.ime}</h3>
-            <h2>Ime predmeta</h2>
-            <h1>{props.ocena}</h1>
+        <div className="ocena">
+            <h3>Študent: {ime} {priimek}</h3>
+            <h4>Leto rojstva: {letnica}</h4>
+            <h2>Ime predmeta: {predmet}</h2>
+            <h1>Ocena: {vrednost}</h1>
+            <button onClick={povecaj}>Povečaj</button>
+            <button onClick={pomanjsaj}>Pomanjšaj</button>
         </div>
     );    
 }
 
 export default Ocene;
+export const vss = "Najjaca šola";
